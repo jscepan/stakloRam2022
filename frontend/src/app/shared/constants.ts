caf1f4a809dc023a4df8fd26b4cecf06b65fc4b2
scepan_jankovic <scepan.jankovic@material-exchange.com>
2022-08-28 04:16:53 +0200

    Ended checking of form data on BE

--- frontend/src/app/shared/constants.ts
+++ frontend/src/app/shared/constants.ts
@@ -9,9 +9,6 @@ export const DOMAIN_INCOMES: string = 'incomes';
 export const DOMAIN_OUTCOMES: string = 'outcomes';
 export const DOMAIN_CITY: string = 'cities';
 export const DOMAIN_IMAGES: string = 'images';
-export const DOMAIN_TASKS: string = 'tasks';
-export const DOMAIN_SUBTASKS: string = 'subtasks';
-export const DOMAIN_CASES: string = 'cases';
 export const DOMAIN_INVOICES: string = 'invoices';
 export const DOMAIN_ROLES: string = 'roles';
 export const DOMAIN_WORK_ORDERS: string = 'workorders';
@@ -31,19 +28,6 @@ export const LOGIN_FORM_CONTENT_TYPE: { 'Content-Type': string } = {
 export const BUYER_TYPES: EnumValueModel[] = [
   { value: 'COMPANY', displayName: 'company' },
   { value: 'PERSON', displayName: 'person' },
-];
-
-export const SERVICE_STATUSES: EnumValueModel[] = [
-  { value: 'OPEN', displayName: 'open' },
-  { value: 'IN_PROGRESS', displayName: 'inProgress' },
-  { value: 'RESOLVED', displayName: 'resolved' },
-  { value: 'CANCELED', displayName: 'canceled' },
-];
-
-export const SERVICE_TYPES: EnumValueModel[] = [
-  { value: 'CASE', displayName: 'case' },
-  { value: 'TASK', displayName: 'task' },
-  { value: 'SUBTASK', displayName: 'subtask' },
 ];
 
 export const GENDER_TYPES: EnumValueModel[] = [
